144bb322783bbc93ac2114c32bbe4024b2b8c968
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-08-17 14:22:09 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/vishal/whatsorder/MainActivity.java
+++ android/app/src/main/java/com/vishal/whatsorder/MainActivity.java
@@ -2,16 +2,12 @@ package com.vishal.whatsorder;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Bridge;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Bridge bridge = this.getBridge();
-        bridge.getWebView().getSettings().setAllowFileAccess(true);
-        bridge.getWebView().getSettings().setAllowFileAccessFromFileURLs(true);
-        bridge.getWebView().getSettings().setAllowUniversalAccessFromFileURLs(true);
+        // Capacitor automatically sets up the WebView.
+        // No need to manually configure Bridge or WebView here.
     }
 }
