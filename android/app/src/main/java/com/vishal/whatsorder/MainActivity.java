e1973b9c6a3938666d5e9d2e32d7207505b71df3
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-08-17 14:10:14 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/vishal/whatsorder/MainActivity.java
+++ android/app/src/main/java/com/vishal/whatsorder/MainActivity.java
@@ -1,15 +1,15 @@
-
 package com.vishal.whatsorder;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Bridge;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // Enable file upload support in WebView
+
+        Bridge bridge = this.getBridge();
         bridge.getWebView().getSettings().setAllowFileAccess(true);
         bridge.getWebView().getSettings().setAllowFileAccessFromFileURLs(true);
         bridge.getWebView().getSettings().setAllowUniversalAccessFromFileURLs(true);
