97aa7c5051b870cda58bc80b8c4b894a85503226
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-08-24 16:56:57 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/vishal/whatsorder/MainActivity.java
+++ android/app/src/main/java/com/vishal/whatsorder/MainActivity.java
@@ -1,22 +1,8 @@
 package com.vishal.whatsorder;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import com.getcapacitor.community.admob.AdMob;
-import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
-
-import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Initializes the Bridge and plugins to be used locally
-        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-            add(GoogleAuth.class);
-            add(AdMob.class);
-        }});
-    }
+    // ✅ Nothing else needed in Capacitor 5
+    // Plugins are registered automatically
 }
